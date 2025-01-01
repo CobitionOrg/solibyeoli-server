2a3436c92d0780d057a06a1f408330d8df1e023d
cxzaqq <roy1228@naver.com>
2025-01-01 16:18:03 +0900

    apioperation 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { SignInDto } from './dto/signIn.dto';
 import { SignUpDto } from './dto/signUp.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
@@ -14,6 +15,7 @@ export class UsersController {
      * 성공: { success: true, status: 200, token: access_token }
      * 실패: { success: false, status: 401 }
      */
+    @ApiOperation({ summary: '사용자 로그인' })
     @Post('/signIn')
     async signnIn(@Body() signInDto: SignInDto) {
         return await this.usersService.signIn(signInDto);
@@ -26,6 +28,7 @@ export class UsersController {
      * 성공: { success: true, status: 200 }
      * 실패: { success: false, status: 409 }
      */
+    @ApiOperation({ summary: '사용자 회원가입' })
     @Post('/signUp')
     async signUp(@Body() signUpDto: SignUpDto) {
         return await this.usersService.signUp(signUpDto);
