12f5cb460c7dc6bc35b9f4d6b173b5fbf009eca4
chobkyu <qudqud97@naver.com>
2025-01-05 18:05:03 +0900

    충돌 해결

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,38 +5,30 @@ import { LoggerMiddleware } from './middleware/logger.middleware';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './filter/httpExceptionFilter';
-import { WordController } from './word/word.controller';
-import { WordService } from './word/word.service';
 import { WordModule } from './word/word.module';
 import { UsersModule } from './users/users.module';
-
-import { ScoreController } from './score/score.controller';
-import { ScoreService } from './score/score.service';
 import { ScoreModule } from './score/score.module';
-
-
 import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    WordModule,
-    UsersModule,
-    AdminModule,
-    ScoreModule,
-  ],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    Logger,
-    {
-      provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
-    },
-  ],
-
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        WordModule,
+        UsersModule,
+        AdminModule,
+        ScoreModule,
+    ],
+    controllers: [AppController],
+    providers: [
+        AppService,
+        Logger,
+        {
+            provide: APP_FILTER,
+            useClass: HttpExceptionFilter,
+        },
+    ],
 })
 export class AppModule {
     configure(consumer: MiddlewareConsumer) {
